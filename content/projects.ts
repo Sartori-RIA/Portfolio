abcfa45e4f98d10cae31edd14c1d0505943ba267
Lucas A. R. Sartori <lucas.a.r.sartori@gmail.com>
2026-06-25 11:48:24 -0300

    feat: added new side project

--- content/projects.ts
+++ content/projects.ts
@@ -7,7 +7,11 @@ export const projects: ProjectItem[] = [
     description:
       "Open-source platform to manage restaurant orders, inventory, and kitchen panels, " +
       "featuring real-time updates, role-based access, and customizable UI.",
-    techs: ["Ruby on Rails", "PostgreSQL", "Angular", "Angular Material", "NgRX", "JWT", "REST APIs"],
+    techs: [
+      "Ruby on Rails", "PostgreSQL", "SideKiq", "SearchKick", "OpenSearch", "AnyCable", "Argon2",
+      "React", "NextJS", "TailwindCSS", "Redux",
+      "Angular", "Angular Material", "NgRX", "JWT", "REST APIs"
+    ],
   },
   {
     name: "ICOS",
@@ -15,4 +19,10 @@ export const projects: ProjectItem[] = [
     description: "NGO project to help the community with free, quality dental care.",
     techs: ["React", "NextJS", "TypeScript"],
   },
+  {
+    name: "Juntos Family",
+    href: "https://juntos-family.com",
+    description: "Celebrate together, even from afar. Invites, wishlists, and Secret Santa — simple, beautiful, and built for your phone.",
+    techs: ["Ruby on Rails", "PostgreSQL", "TailwindCSS"],
+  }
 ];
